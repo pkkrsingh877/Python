eac6ea71ca52394894828a2a7ed6c51c944e7a0a
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-03-16 18:45:24 +0530

    Get user input in celsius and check for temperature being greater than 30 deg, lesser than 10 deg and otherwise.

---
+++ If Else/temp.py
@@ -0,0 +1,8 @@
+celsius = int(input('Enter Temperature in Celsius: '));
+
+if celsius > 30:
+    print("It's a hot day!")
+elif celsius < 10:
+    print("It's a cold day!")
+else:
+    print("It's neither hot nor cold.")
